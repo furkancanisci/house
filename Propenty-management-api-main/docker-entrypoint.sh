1f832bfe6122e37a181f859f7675de85a125e4bc
Furkan Can İşci <furkancanisci@outlook.com>
2025-07-31 10:36:40 +0300

    fix deploy

--- Propenty-management-api-main/docker-entrypoint.sh
+++ Propenty-management-api-main/docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e  # Script hata alırsa durur, güvenli olur
 
+php artisan config:clear
+php artisan cache:clear
+
 
 echo " Migrationlar çalıştırılıyor..."
 if ! php artisan migrate --force; then
